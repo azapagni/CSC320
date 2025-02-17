af429aa55ac4723fbef9aab83510ffa423077a9c
Aza <azapagni@gmail.com>
2025-02-16 21:22:08 -0800

    Add files via upload

---
+++ AddressBook.java
@@ -0,0 +1,18 @@
+package csc320;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print ("The first name is ");
+		System.out.println ("John");
+		System.out.print ("The last name is ");
+		System.out.println ("Smith");
+		System.out.println ("The address is:");
+		System.out.println ("123 Main Street");
+		System.out.println ("City");
+		System.out.println ("55555");
+
+	}
+
+}
